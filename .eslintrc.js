a725ecc63e094308e1a6070a3e9323fbabd2ce59
Ali Karbassi <ali@karbassi.com>
2023-05-08 14:40:35 -0500

    WIP

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,15 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": 0,
-        "prefer-const": 2
-    }
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-console": 0,
+    "prefer-const": 2,
+  },
 };
